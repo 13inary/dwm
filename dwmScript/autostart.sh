914b224d8cad88f8cd18dcb0cf99b00a02e230b1
iamclient <936095897@qq.com>
2025-04-04 18:23:07 +0800

    clash: remove manual run

--- dwmScript/autostart.sh
+++ dwmScript/autostart.sh
@@ -9,9 +9,9 @@ firefox &
 #	/usr/local/dwmScript/bar-message.sh
 #	sleep 1m
 #done &
-#st -d ~/ -e bash -c "ranger --choosedir=$HOME/.config/ranger/.rangerdir;exec bash"; 
+#st -d ~/ -e bash -c "ranger --choosedir=$HOME/.config/ranger/.rangerdir;exec bash";
 #xsetroot -name "Wifi:$(cat /sys/class/net/<你的无线网卡名>/operstate)|Ethernet:$(cat /sys/class/net/<你的有线网卡名>/operstate)|Battery:$(acpi -b | awk '{print $4}' | cut -d"," -f1)|Volume:$(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }')|$(LC_ALL='C' date +'%F[%b %a] %R')" %#在dwm的菜单栏里显示一个简易的系统状态栏，包括wifi、有线网、电池电量、音量、日期和时间(24小时制)。喜欢功能丰富的同学可以自己去安装配置polybar、conky、i3status、slstatus等，从中选一个。
-#xset dpms 1800 0 
+#xset dpms 1800 0
 /usr/local/dwmScript/touchpad-switch.sh
 /usr/local/dwmScript/sound-init.sh &
 
@@ -28,4 +28,4 @@ mkdir -p /tmp/screenshot_tmp
 # sed -i "/external-controller:/d" ~/.config/clash/config.yaml
 # sed -i "/mixed-port:/d" ~/.config/clash/config.yaml
 # sed -i "1i mixed-port: 7890" ~/.config/clash/config.yaml
-clash > /var/log/clash/clash.log &
+#clash > /var/log/clash/clash.log &
